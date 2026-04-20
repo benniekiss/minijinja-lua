239029672f9a30b18a168ffff54a2781e1cc6ae2
benniekiss <63211101+benniekiss@users.noreply.github.com>
2026-04-20 10:36:16 -0400

    chore(examples): add nested context test

--- examples/spec/minijinja_pandoc_filter_spec.lua
+++ examples/spec/minijinja_pandoc_filter_spec.lua
@@ -1,8 +1,8 @@
 local filter = require("minijinja_pandoc_filter")
 
 describe("Examples", function()
-    describe("pandoc#examples", function()
-        it("minijinja filter#examples", function()
+    describe("minijinja_pandoc_filter#examples", function()
+        it("simple#examples", function()
             local source = [[
 ---
 minijinja:
@@ -11,6 +11,28 @@ minijinja:
 ---
 
 Test: {{ foo }}
+]]
+            local doc = pandoc.read(source, "markdown")
+
+            local te = doc:walk(filter)
+            local ex = pandoc.read(
+                [=[[ Para [Str "Test:",Space,Str "BOO"]]]=],
+                "native"
+            )
+
+            assert.Equal(ex, te)
+        end)
+
+        it("nested context#examples", function()
+            local source = [[
+---
+minijinja:
+    context:
+        foo:
+            bar: "BOO"
+---
+
+Test: {{ foo.bar }}
 ]]
             local doc = pandoc.read(source, "markdown")
 
