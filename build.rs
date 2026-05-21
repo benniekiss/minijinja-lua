03a0a43a744d903fde42ef4f0299799f6906cdf6
benniekiss <63211101+benniekiss@users.noreply.github.com>
2026-05-21 15:17:55 -0400

    fix: -Z flags cannot be used in build scripts

--- build.rs
+++ build.rs
@@ -1,9 +1,4 @@
 fn main() {
-    if std::env::var("PROFILE").unwrap_or_default() == "release" {
-        println!("cargo:rustc-flags=-Zlocation-detail=none");
-        println!("cargo:rustc-flags=-Zfmt-debug=none");
-    }
-
     if cfg!(target_os = "macos") {
         println!("cargo:rustc-cdylib-link-arg=-undefined");
         println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
