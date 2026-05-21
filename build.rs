96501f69cd9e11c8fa22bbb4865f9194cf1137b2
benniekiss <63211101+benniekiss@users.noreply.github.com>
2026-05-21 15:09:28 -0400

    fix: copy-paste error

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
-    if cfg!(target_os = "macos") {
-        println!("cargo:rustc-cdylib-link-arg=-undefined");
-        println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
+    if std::env::var("PROFILE").unwrap_or_default() == "release" {
+        println!("cargo:rustc-flags=-Zlocation-detail=none");
+        println!("cargo:rustc-flags=-Zfmt-debug=none");
     }
 
     if cfg!(target_os = "macos") {
